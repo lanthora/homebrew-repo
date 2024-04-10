f1e565cad7ef3f32c58bfb35b05a183e1e3dfcce
lanthora <lanthora@outlook.com>
2024-04-10 18:04:57 +0800

    candy: update to v5.5

--- Formula/candy.rb
+++ Formula/candy.rb
@@ -1,17 +1,18 @@
 class Candy < Formula
   desc "Another virtual private network that supports peer-to-peer connections"
   homepage "https://github.com/lanthora/candy"
-  url "https://github.com/lanthora/candy/archive/v5.4.tar.gz"
-  sha256 "d2592f9ab582ad09b2c990ec162bd552b0a1d511667bbad49f4cdd98ac828a90"
+  url "https://github.com/lanthora/candy/archive/v5.5.tar.gz"
+  sha256 "d58093ccca684ddef22e4051b4aa42180282fbd232f085021005394d9cbc317d"
   license "MIT"
   head "https://github.com/lanthora/candy.git"
 
   depends_on "cmake" => :build
   depends_on "argp-standalone" => :build
-  depends_on "uriparser" => :build
   depends_on "pkg-config" => :build
+  depends_on "uriparser"
   depends_on "libconfig"
   depends_on "openssl"
+  depends_on "poco"
   depends_on "spdlog"
 
   def install
