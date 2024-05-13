f2cf0eab4381edb87e62f0ac8460dc37d7bf10cb
lanthora <lanthora@outlook.com>
2024-05-13 17:14:32 +0800

    candy: update to v5.8.2

--- Formula/candy.rb
+++ Formula/candy.rb
@@ -1,8 +1,8 @@
 class Candy < Formula
   desc "A reliable, low-latency, and anti-censorship virtual private network"
   homepage "https://github.com/lanthora/candy"
-  url "https://github.com/lanthora/candy/archive/v5.8.1.tar.gz"
-  sha256 "3ee9d106a17462f7204096289b2b0240d3dea29f647cd0e629b1504a974a31ca"
+  url "https://github.com/lanthora/candy/archive/v5.8.2.tar.gz"
+  sha256 "56e6a8af3353ceed10204804cfe2dfc7aff4bbf0fde718246496c997048c9264"
   license "MIT"
   head "https://github.com/lanthora/candy.git"
 
@@ -19,11 +19,11 @@ class Candy < Formula
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-    etc.install "candy.conf"
+    etc.install "candy.cfg"
   end
 
   service do
-    run [opt_bin/"candy", "-c", etc/"candy.conf"]
+    run [opt_bin/"candy", "-c", etc/"candy.cfg"]
     keep_alive true
     require_root true
     error_log_path var/"log/candy.log"
