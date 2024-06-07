d535f54dfc07ec35e43457633df64bbad6d9a557
lanthora <lanthora@outlook.com>
2024-06-07 15:59:01 +0800

    candy: update to v5.8.7

--- Formula/candy.rb
+++ Formula/candy.rb
@@ -1,15 +1,14 @@
 class Candy < Formula
   desc "A reliable, low-latency, and anti-censorship virtual private network"
   homepage "https://github.com/lanthora/candy"
-  url "https://github.com/lanthora/candy/archive/v5.8.6.tar.gz"
-  sha256 "b928a9c4a807f8badae3d967eba372c3130f1421e8096402b21cb22d14c260c6"
+  url "https://github.com/lanthora/candy/archive/v5.8.7.tar.gz"
+  sha256 "50aeb5371fab0e259e785b94bfa5e90e7a14f9bb0344cf6ec4fb1eee0ca049ec"
   license "MIT"
   head "https://github.com/lanthora/candy.git"
 
   depends_on "cmake" => :build
   depends_on "argp-standalone" => :build
   depends_on "pkg-config" => :build
-  depends_on "uriparser"
   depends_on "libconfig"
   depends_on "openssl"
   depends_on "poco"
