52018666c22df54e2a32b2f05274ba4385ccbf0c
lanthora <lanthora@outlook.com>
2024-09-12 10:29:52 +0800

    candy: update to v5.10.4

--- Formula/candy.rb
+++ Formula/candy.rb
@@ -1,8 +1,8 @@
 class Candy < Formula
   desc "A reliable, low-latency, and anti-censorship virtual private network"
   homepage "https://github.com/lanthora/candy"
-  url "https://github.com/lanthora/candy/archive/v5.10.3.tar.gz"
-  sha256 "defb1b75ebc9b17cd448a917e9879a8324ca76869f9cc3035419764ec1826954"
+  url "https://github.com/lanthora/candy/archive/v5.10.4.tar.gz"
+  sha256 "55d7893d939a3bf1019973ee38b4e6357d49cbf0880a8125953fc1789fa58fb0"
   license "MIT"
   head "https://github.com/lanthora/candy.git"
 
